9144f98228e568c3f9446756643dbf529c05cfbe
Muhammad Ahmed Aleem <muhammadaleem@Muhammads-MacBook-Pro.local>
2020-11-08 17:52:49 -0500

    Create GraphQL App

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema =  require('./schema');
+const app = express();
+ 
+
+//From GraphQL github
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true
+    })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
